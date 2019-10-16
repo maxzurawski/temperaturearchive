f4da1fa15a11bd94f4530e73a16039c45b2b715f
Max Zurawski <maksymilian.zurawski@consileon.pl>
2019-10-16 14:47:10 +0200

    Find func

---
+++ service/find.go
@@ -0,0 +1,15 @@
+package service
+
+import "github.com/xdevices/temperaturearchive/dto"
+
+func (s *service) Find(searchDTO dto.SearchDTO) ([]dto.MeasurementDTO, error) {
+	measurements, err := s.mgr.Find(searchDTO)
+	if err != nil {
+		return nil, err
+	}
+	var results []dto.MeasurementDTO
+	for _, item := range measurements {
+		results = append(results, s.mgr.MapToDto(&item))
+	}
+	return results, nil
+}
