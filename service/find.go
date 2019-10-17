594cfcaf459f5ffd1d13e0b963e1d6ca94e5a2ad
Max Zurawski <maksymilian.zurawski@consileon.pl>
2019-10-17 17:46:49 +0200

    Reverting publishing of an error during search

--- service/find.go
+++ service/find.go
@@ -1,23 +1,12 @@
 package service
 
 import (
-	"encoding/json"
-	"fmt"
-
-	"github.com/google/uuid"
 	"github.com/xdevices/temperaturearchive/dto"
-	"github.com/xdevices/temperaturearchive/publishers"
 )
 
 func (s *service) Find(searchDTO dto.SearchDTO) ([]dto.MeasurementDTO, error) {
 	measurements, err := s.mgr.Find(searchDTO)
 	if err != nil {
-		bytes, _ := json.Marshal(searchDTO)
-		publishers.Logger().Error(
-			uuid.New().String(),
-			"",
-			fmt.Sprintf("error during finding of measurements. search dto: [%s]", string(bytes)),
-			err.Error())
 		return nil, err
 	}
 	var results []dto.MeasurementDTO
