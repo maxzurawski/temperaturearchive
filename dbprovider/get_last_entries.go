0b0d4dd10fd163d9fdc5a2994a6672b9ca4aed79
Max Zurawski <maksymilian.zurawski@consileon.pl>
2019-11-03 23:06:25 +0100

    Extending GetLastEntries with additional parameter, to get last entries different then given processId.

--- dbprovider/get_last_entries.go
+++ dbprovider/get_last_entries.go
@@ -2,8 +2,10 @@ package dbprovider
 
 import "github.com/maxzurawski/temperaturearchive/model"
 
-func (mgr *manager) GetLastEntries(amount int, uuid string) ([]model.Measurement, error) {
+func (mgr *manager) GetLastEntries(amount int, uuid, processid string) ([]model.Measurement, error) {
 	var results []model.Measurement
-	error := mgr.GetDb().Where("uuid = ?", uuid).Limit(amount).Order("reported_at desc").Find(results).Error
+	error := mgr.GetDb().Where("uuid = ?", uuid).
+		Where("process_id <> ?", processid).
+		Limit(amount).Order("reported_at desc").Find(results).Error
 	return results, error
 }
