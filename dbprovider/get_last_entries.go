abfcbe66e79bcbb87e388145b96468241d62842f
Max Zurawski <maksymilian.zurawski@consileon.pl>
2019-11-07 09:48:55 +0100

    Checking if this will fix the problem with failing processors in temperature archive.

--- dbprovider/get_last_entries.go
+++ dbprovider/get_last_entries.go
@@ -6,6 +6,6 @@ func (mgr *manager) GetLastEntries(amount int, uuid, processid string) ([]model.
 	var results []model.Measurement
 	error := mgr.GetDb().Where("uuid = ?", uuid).
 		Where("process_id <> ?", processid).
-		Limit(amount).Order("reported_at desc").Find(results).Error
+		Limit(amount).Order("reported_at desc").Find(&results).Error
 	return results, error
 }
